3b84b0d3ef364ba854052678c55beac1d864046a
Emil Wennberg <emil.wennberg@inceptive.com>
2018-05-21 16:01:28 +0200

    Add default todos (duplicates will be created) and hide todo list when no portcall is selected

--- App/components/checkbox-view/AddComponent.js
+++ App/components/checkbox-view/AddComponent.js
@@ -21,8 +21,21 @@ export default class AddComponent extends Component {
     constructor (props) {
         super(props);
         this.state = ({
-            newTaskName: ''
+            newTaskName: '',
+            initiated: false
         });
+        console.log(this.props.tasks);
+        if(!this.state.initiated){
+            this.addCheckBox("Book tugboat");
+            this.addCheckBox("Book pilot");
+            this.addCheckBox("Book port");
+            this.addCheckBox("Book slugboat");
+            this.state.initiated = true;
+            console.log(this.state.initiated);
+        }
+
+
+
     }
 
 
@@ -32,7 +45,10 @@ export default class AddComponent extends Component {
         }
         // Call click event => use 'Container', look at AddContainer for additional information.
         this.props.onClickAdd(this.state.newTaskName);
+    }
 
+    addCheckBox(name) {
+        this.props.onClickAdd(name);
     }
 
 
